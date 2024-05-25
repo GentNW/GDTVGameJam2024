5f1a27dbeadab4782223b19d7aab779006ac7bfa
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-05-26 01:12:47 +0300

    Initial Commit

---
+++ Source/GameDevtvGameJam2024/GameDevtvGameJam2024.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameDevtvGameJam2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameDevtvGameJam2024, "GameDevtvGameJam2024" );
